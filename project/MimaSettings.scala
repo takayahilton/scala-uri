54d2c7c659a92555872ccc78cb501dc6cb0d7f97
ta.tanaka <takayahilton@gmail.com>
2019-06-13 18:06:41 +0900

    [wip] Added sbt-mima-plugin to validate binary incompatibility.

---
+++ project/MimaSettings.scala
@@ -0,0 +1,22 @@
+import sbt._, Keys._
+import com.typesafe.tools.mima.plugin.MimaPlugin
+import com.typesafe.tools.mima.plugin.MimaKeys.{mimaPreviousArtifacts, mimaReportBinaryIssues}
+
+
+object MimaSettings {
+  val previousVersions = (0 to 5).map(patch => s"1.4.$patch").toSet
+
+  val mimaSettings = MimaPlugin.mimaDefaultSettings ++ Seq(
+    mimaPreviousArtifacts := {
+      CrossVersion.partialVersion(scalaVersion.value) match {
+        case Some((2, scalaMajor)) if scalaMajor <= 12 =>
+          previousVersions.map { organization.value % s"${name.value}_${scalaBinaryVersion.value}" % _ }
+        case _ => Set.empty
+      }
+    },
+    test in Test := {
+      mimaReportBinaryIssues.value
+      (test in Test).value
+    }
+  )
+}
